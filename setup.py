4ca471be6d9f2ce45fd3efb9e3b11da9ca858c37
Philippe O. Wagner <wagner@arteria.ch>
2014-08-29 23:04:37 +0200

    Added more package info.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 
 setup(
     name = "threebot-crypto",
-    version = "1.0.0",
-    author_email = "",
-    maintainer_email = "",
+    version = "1.0.1",
+    author_email = "wagner@arteria.ch",
+    maintainer_email = "admin@arteria.ch",
     install_requires=[
         "pycrypto>=2.6.1",
     ],
