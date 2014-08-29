6f93a42200f9d54b2d315251551ee47f38563067
Philippe O. Wagner <wagner@arteria.ch>
2014-08-30 00:32:24 +0200

    Added missing package data

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup
 
 setup(
     name = "threebot-crypto",
-    version = "1.0.1",
+    packages = ['threebot_crypto'],
+    version = "1.0.3",
     author_email = "wagner@arteria.ch",
     maintainer_email = "admin@arteria.ch",
     install_requires=[
