29869984fc1016294a2c96c948b73ca893afaf9b
Walter Renner <renner@arteria.ch>
2017-08-02 11:16:45 +0200

    Major Version Bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="threebot-crypto",
     packages=['threebot_crypto'],
-    version="1.0.16",
+    version="1.1.0",
     author_email="wagner@arteria.ch",
     maintainer_email="admin@arteria.ch",
     install_requires=open('requirements.txt').read().split('\n'),
